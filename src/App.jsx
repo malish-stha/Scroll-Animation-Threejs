9b562a834b63b4d608020d4f4e2e06db5ab69565
malish-stha <malish.shrestha@deerwalk.edu.np>
2023-12-30 08:37:53 +0545

    Update dependencies, add gsap, update package.json, update App.jsx, update Experience.jsx, update vite.config.js, add Office component, add scroll controls, update camera position.

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,12 @@ import Experience from "./components/Experience";
 
 function App() {
   return (
-    <Canvas>
+    <Canvas
+      camera={{
+        fov: 64,
+        position: [2.3, 1.5, 2.3],
+      }}
+    >
       <Experience />
     </Canvas>
   );
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,14 +1,14 @@
-import { OrbitControls } from "@react-three/drei";
-import React from "react";
+import { OrbitControls, ScrollControls } from "@react-three/drei";
+import Office from "./Office";
 
 const Experience = () => {
   return (
     <>
-      <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <ambientLight intensity={1} />
+      <OrbitControls enableZoom={false} />
+      <ScrollControls pages={3} damping={0.25}>
+        <Office />
+      </ScrollControls>
     </>
   );
 };
